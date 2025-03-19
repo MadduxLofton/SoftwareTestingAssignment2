f654bda6bbd48de62304a4c4ea246a99e8f8f6a8
MadduxLofton <125148911+MadduxLofton@users.noreply.github.com>
2025-03-19 18:28:09 -0500

    Add files via upload

--- SW_Testing_assignment_2.py
+++ SW_Testing_assignment_2.py
@@ -9,7 +9,20 @@ def classify(bmi):
         return "Obese"
     
 def find_BMI(height, weight):
-    return 0
+    BMI = (weight / (height) ** 2) * 703
+    return BMI
 
 def check_num(num_val):
-    return 0
+    if num_val.isnumeric():
+        return True
+    else:
+        for i in range(len(num_val)):
+            temp = num_val[i]
+            if not (temp == "1" or temp == "2" or temp == "3" or temp == "4" or temp == "5" or temp == "6" or temp == "7" or temp == "8" or temp == "9" or temp == "0" or temp == "."):
+                return False
+            num2 = float(num_val)
+            num_int = int(num2)
+            num3 = num2 - num_int
+            if num3 < 0.1:
+                return False
+            return True
--- unit tests.py
+++ unit tests.py
@@ -37,13 +37,20 @@ class TestBMIProgram(unittest.TestCase):
         self.assertEqual(main.classify(30), "Obese")
     
     def test_input_validation(self):
-        self.assertEqual(main.check_num(1), True)
-        self.assertEqual(main.check_num(1.1), True)
-        self.assertEqual(main.check_num(1.01), False) # 0.1 is the smallest allowable change
         self.assertEqual(main.check_num("apple"), False)
         self.assertEqual(main.check_num("1"), True)
+        self.assertEqual(main.check_num("0.1"), True)
         self.assertEqual(main.check_num("1.1"), True)
-        self.assertEqual(main.check_num("1.02"), False)
+        self.assertEqual(main.check_num("1.01"), False) # Smallest change allowed is 0.1
+        self.assertEqual(main.check_num("1.09"), False) # Smallest change allowed is 0.1
+
+    def test_BMI_calculation(self):
+        # find_BMI(height in inches, weight in pounds)
+        self.assertEqual(main.find_BMI(50, 100), 28.1)
+        self.assertEqual(main.find_BMI(60, 130), 25.4)
+        self.assertEqual(main.find_BMI(70, 175), 25.1)
+        self.assertEqual(main.find_BMI(80, 300), 33)
+
 
 if __name__ == '__main__':
     unittest.main()
